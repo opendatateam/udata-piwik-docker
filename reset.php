92f89b0c1071cae880fa515ebd99d8913a62fd26
Alexandre Bulté <alexandre@bulte.net>
2017-12-13 15:24:29 +0100

    Reset.php hack

---
+++ reset.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Reset all visits.
+ * /!\ TOTALLY UNSAFE, use only for dev.
+ */
+
+if (!defined('PIWIK_DOCUMENT_ROOT')) {
+    define('PIWIK_DOCUMENT_ROOT', dirname(__FILE__) == '/' ? '' : dirname(__FILE__));
+}
+if (file_exists(PIWIK_DOCUMENT_ROOT . '/bootstrap.php')) {
+    require_once PIWIK_DOCUMENT_ROOT . '/bootstrap.php';
+}
+if (!defined('PIWIK_INCLUDE_PATH')) {
+    define('PIWIK_INCLUDE_PATH', PIWIK_DOCUMENT_ROOT);
+}
+
+require_once PIWIK_INCLUDE_PATH . '/core/bootstrap.php';
+
+use Piwik\DataAccess\RawLogDao;
+use Piwik\LogDeleter;
+use Piwik\Container\StaticContainer;
+use Piwik\FrontController;
+
+use Piwik\Db;
+use Piwik\Common;
+
+FrontController::setUpSafeMode();
+$environment = new \Piwik\Application\Environment(null);
+try {
+        $environment->init();
+} catch(\Exception $e) {}
+
+$controller = FrontController::getInstance();
+$controller->init();
+
+$logDeleter = StaticContainer::get('Piwik\LogDeleter');
+$rawLogDao = StaticContainer::get('Piwik\DataAccess\RawLogDao');
+
+$logDeleter->deleteVisitsFor(null, null);
+
+print('OK');
